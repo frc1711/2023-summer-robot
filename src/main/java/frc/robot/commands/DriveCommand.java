frc1711/2023-summer-robot

commit 4704fcccaba00da5edcf8cc7f9958d65270027ae

Fixed ignored drive deadbands.

src/main/java/frc/robot/commands/DriveCommand.java
@@ -59,55 +59,67 @@ public class DriveCommand extends CommandBase {
   @Override
   public void execute() {
 
-    if (slowMode.getAsBoolean()) {
-      speedMultiplier = .25;
-    }
-    else { 
-      speedMultiplier = 1;
-    }
+    double transformedXSpeed = xSpeed.getAsDouble();
+    double transformedYSpeed = ySpeed.getAsDouble();
+    double transformedThetaSpeed = thetaSpeed.getAsDouble();
 
-    if (turnAround.getAsBoolean() && !wasOneEighty) { 
+    double xDeadband = 0.15;
+    double yDeadband = 0.15;
+    double thetaDeadband = 0.15;
+
+    if (transformedXSpeed < xDeadband) transformedXSpeed = 0;
+    if (transformedYSpeed < yDeadband) transformedYSpeed = 0;
+    if (transformedThetaSpeed < thetaDeadband) transformedThetaSpeed = 0;
+
+    this.speedMultiplier = slowMode.getAsBoolean() ? 0.25 : 1;
+
+    if (turnAround.getAsBoolean() && !wasOneEighty) {
+
       timer.reset();
       oneEighty = 1;
       wasOneEighty = true;
-    }
-    else if (oneEighty != 0 && timer.hasElapsed(1)) {
+
+    } else if (oneEighty != 0 && timer.hasElapsed(1)) {
+
       oneEighty = 0;
       wasOneEighty = false;
+
     }
 
-    if (resetGyro.getAsBoolean()) {
-      swerveSubsystem.resetGyro();
-    }
+    if (resetGyro.getAsBoolean()) swerveSubsystem.resetGyro();
 
-    if (xMode.getAsBoolean()) {
-      swerveSubsystem.xMode();
-    }
+    if (xMode.getAsBoolean()) swerveSubsystem.xMode();
+    else if (
+      Math.abs(transformedXSpeed) > .15 ||
+      Math.abs(transformedYSpeed) > .15 ||
+      Math.abs(transformedThetaSpeed) > .15
+    ) {
 
-    else if (Math.abs(xSpeed.getAsDouble()) > .15 || 
-        Math.abs(ySpeed.getAsDouble()) > .15 || 
-        Math.abs(thetaSpeed.getAsDouble()) > .15 && 
-        !xMode.getAsBoolean()) {
-          if (thetaSpeed.getAsDouble() < 0) turnSpeed = thetaSpeed.getAsDouble();
-          else turnSpeed = thetaSpeed.getAsDouble() * .5;
+      this.turnSpeed = transformedThetaSpeed;
+
       swerveSubsystem.updateModules(
         ChassisSpeeds.fromFieldRelativeSpeeds(
-          (xSpeed.getAsDouble()), 
-          (ySpeed.getAsDouble()), 
-          (turnSpeed) + oneEighty, 
-          swerveSubsystem.getGyroRotation()),
-          speedMultiplier);
-    }
+          transformedXSpeed,
+          transformedYSpeed,
+          this.turnSpeed + oneEighty,
+          swerveSubsystem.getGyroRotation()
+        ),
+        speedMultiplier
+      );
 
-    else {
-    swerveSubsystem.stop();
+    } else {
+
+      swerveSubsystem.stop();
       swerveSubsystem.updateModules(
         ChassisSpeeds.fromFieldRelativeSpeeds(
           0,
           0,
           oneEighty, 
-          swerveSubsystem.getGyroRotation()),
-          1);
+          swerveSubsystem.getGyroRotation()
+        ),
+        1
+      );
+
   }
 
 }

commit 93acdf95ed195f45a73fd2301c54d74bb0dce27d

Fixed absolute value bug introduced by 4704fccc.

src/main/java/frc/robot/commands/DriveCommand.java
@@ -67,9 +67,9 @@ public class DriveCommand extends CommandBase {
     double yDeadband = 0.15;
     double thetaDeadband = 0.15;
 
-    if (transformedXSpeed < xDeadband) transformedXSpeed = 0;
-    if (transformedYSpeed < yDeadband) transformedYSpeed = 0;
-    if (transformedThetaSpeed < thetaDeadband) transformedThetaSpeed = 0;
+    if (Math.abs(transformedXSpeed) < xDeadband) transformedXSpeed = 0;
+    if (Math.abs(transformedYSpeed) < yDeadband) transformedYSpeed = 0;
+    if (Math.abs(transformedThetaSpeed) < thetaDeadband) transformedThetaSpeed = 0;
 
     this.speedMultiplier = slowMode.getAsBoolean() ? 0.25 : 1;
 