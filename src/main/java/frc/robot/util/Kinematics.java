a5b87f586a8b7d6f070d8c5cc56cc205962c22c9
David <98991028+ActualSkinwalker@users.noreply.github.com>
2023-11-30 18:33:12 -0500

    Schizophrenic ramblings of a madman
    Kinematics is going well. Look upon my works, ye mighty, and despair.

--- src/main/java/frc/robot/util/Kinematics.java
+++ src/main/java/frc/robot/util/Kinematics.java
@@ -8,61 +8,124 @@ import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.math.Nat;
 import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /** Add your docs here. */
-public class Kinematics {
+public class Kinematics extends SubsystemBase {
 
-    AHRS gyro;
+        AHRS gyro;
 
-    Timer xAccelTimer, yAccelTimer;
+        Timer xAccelTimer, yAccelTimer, totalTimer;
 
-    public Kinematics (AHRS gyro) {
-        this.gyro = gyro;
-        xAccelTimer = new Timer();
-        yAccelTimer = new Timer();
-        xAccelTimer.start();
-        yAccelTimer.start();
+        StartPosition startPosition;
+
+        public Kinematics (AHRS gyro, StartPosition startPosition) {
+            this.gyro = gyro;
+            this.startPosition = startPosition;
+            xAccelTimer = new Timer();
+            yAccelTimer = new Timer();
+            totalTimer = new Timer();
+            xAccelTimer.start();
+            yAccelTimer.start();
+        }
+
+        //TODO: Find coordinates of start positions
+        public enum StartPosition {
+            WIREGUARD (0, 0),
+            BALANCE (0, 0),
+            INSIDE (0, 0);
+            double xVariable, yVariable;
+            StartPosition (double xVariable, double yVariable) {
+                this.xVariable = xVariable;
+                this.yVariable = yVariable;
+            }
+        }
+
+        public boolean directionHasChanged (Vector vectorOne, Vector vectorTwo) {
+            return getDirectionChange(vectorOne, vectorTwo) >= 1;
+        }
+
+        public double getVelocityX () {
+            return getRobotAccelerationX() * xAccelTimer.get() + 0; //TODO: find constant variable through testing(?)
+        }
+
+        public double getVelocityY () {
+            return getRobotAccelerationY() * yAccelTimer.get() + 0; //TODO: find unknown constant variable
+        }
+
+        public double getRobotAccelerationX () {
+            return gyro.getWorldLinearAccelX();
+        }
+
+        public double getRobotAccelerationY () {
+            return gyro.getWorldLinearAccelY();
+        }
+
+        private double totalDisplacementY;
+        public double getRobotDisplacementY () {
+            return .5 * getRobotAccelerationY() * Math.pow(yAccelTimer.get(), 2) + 0 * yAccelTimer.get() + startPosition.yVariable; //TODO: Find unknown constant either through testing or math
+        }
+
+        private double totalDisplacementX;
+        public double getRobotDisplacementX () {
+            return .5 * getRobotAccelerationX() * Math.pow(xAccelTimer.get(), 2) + 0 * xAccelTimer.get() + startPosition.xVariable; //TODO: Find unknown constant either through testing or math
+        }
+
+        public Vector getDisplacementVector () {
+            return new Vector<>(Nat.N2(), getRobotAccelerationX(), getRobotDisplacementY());
+        }
+
+        private void resetTimerX () {
+            xAccelTimer.reset();
+        }
+        
+        public Vector getAccelerationVector() {
+            return new Vector<>(
+                Nat.N2(),
+                getRobotAccelerationX(),
+                getRobotAccelerationY());
+        }
+
+        public double getDirectionChange (Vector vectorOne, Vector vectorTwo) {
+            return Vector.getAngle(vectorTwo) - Vector.getAngle(vectorOne);
+        }
+
+        Vector position;
+        public void updateRobotPosition (Vector vectorOne, Vector vectorTwo) {
+            if (vectorOne == null) vectorOne = new Vector<>(Nat.N2(), 0, 0);
+            if (vectorTwo == null) vectorTwo = new Vector<>(Nat.N2(), 0, 0);
+            position = vectorOne.add(vectorTwo);
+        }
+
+        public Translation2d getRobotRelativePosition () {
+            return new Translation2d(getRobotDisplacementX(), getRobotDisplacementY());
+        }
+
+        Vector vectorOne = null;
+        Vector vectorTwo = null;   
+        boolean oneHasBeenSet = false;
+        boolean twoHasBeenSet = false;
+        @Override
+        public void periodic() {
+            if (totalTimer.hasElapsed(.1) && !oneHasBeenSet) {
+                vectorOne = getDisplacementVector();
+
+                oneHasBeenSet = true;
+            }
+            if (totalTimer.hasElapsed(.5) && twoHasBeenSet) {
+                vectorTwo = getDisplacementVector();
+
+                twoHasBeenSet = true;
+            }        
+            
+            if (oneHasBeenSet && twoHasBeenSet) {
+                updateRobotPosition(vectorOne, vectorTwo);
+
+                oneHasBeenSet = false;
+                twoHasBeenSet = false;
+                totalTimer.reset();
+            }
+        }
     }
-
-    public boolean directionHasChanged (Vector vectorOne, Vector vectorTwo) {
-        return getDirectionChange(vectorOne, vectorTwo) >= 1;
-    }
-
-    public double getRobotAccelerationX () {
-        return gyro.getWorldLinearAccelX();
-    }
-
-    public double getRobotAccelerationY () {
-        return gyro.getWorldLinearAccelY();
-    }
-
-    private double totalDisplacementY;
-    public double getRobotDisplacementY () {
-        return 0;
-    }
-
-    private double totalDisplacementX;
-    public double getRobotDisplacementX () {
-        return 0;
-    }
-
-    private void resetTimerX () {
-        xAccelTimer.reset();
-    }
-    
-    public Vector getAccelerationVector() {
-        return new Vector<>(
-            Nat.N1(),
-            getRobotAccelerationX(),
-            getRobotAccelerationY());
-    }
-
-    public double getDirectionChange (Vector vectorOne, Vector vectorTwo) {
-        return Vector.getAngle(vectorTwo) - Vector.getAngle(vectorOne);
-    }
-
-    public Transform2d getRobotRelativePosition () {
-        return null;
-    }
-}
